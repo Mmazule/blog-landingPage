bfa3059df538f452099039f9b5d9a968d1b3159b
Mmazule <princessigirigba1@gmaul.com>
2023-03-20 15:18:51 +0100

    initialized files

---
+++ script.js
@@ -0,0 +1,58 @@
+// // // Image slider / carousel
+// // <
+// // script type = "text/javascript" >
+// //     var slide_index = 1;
+// // displaySlides(slide_index);
+
+// // function nextSlide(n) {
+// //     displaySlides(slide_index += n);
+// // }
+
+// // function currentSlie(n) {
+// //     displaySlides(slide_index = n);
+// // }
+
+// // function displaySlides(n) {
+// //     var i;
+// //     var slides = document.getElementByClassName("showSlide")
+// //     if (n > slides.length) {
+// //         slide_index = 1
+// //     }
+// //     if (n < 1) {
+// //         slide_index = slides.length
+// //     }
+// //     for (i = 0; i < slides.length; i++) {
+// //         slides[i].style.display = "none";
+// //     }
+// //     slides[slide_index - 1].style.display = "block"
+// // } <
+// // /script>
+
+// // image slider via W3C schools
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next / previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("slider-fade");
+//     let dot = document.getElementsByClassName("dot");
+
+//     if (n > slides.lenght) { slideIndex = 1 }
+
+//     if (n < 1) { slideIndex = slides.length }
+
+//     for (i = 0; i < DOMStringList.lenght; i++) {
+//         dots[i].className = dots[i].className.replace("active", "");
+//     }
+//     slides[slideIndex - 1].getElementsByClassName.display = "block";
+//     dots[slideIndex - 1].className += "active";
+// }
